26fc4f965390b72a04a0e63771016f61503c9d77
Ægir Örn Símonarson <agirorn@gmail.com>
2011-09-02 21:41:56 +0000

    Tying up and adind some support files.

---
+++ js/bookmarklet.js
@@ -0,0 +1,6 @@
+function() { 
+  var bf =document.createElement('script');
+  bf.type = 'text/javascript';
+  bf.src = 'https://raw.github.com/agirorn/BoldFace/master/js/BoldFace.js';
+  document.getElementsByTagName('head')[0].appendChild(bf);
+})();
